df4c0746f2b6b82a9ceb12e33fea7e2d0405c5e3
myth52125 <myth52125@hotmail.com>
2017-10-19 00:27:40 +0800

    3.3 method 2

--- beauty_of_programming/3.3compareStr.cc
+++ beauty_of_programming/3.3compareStr.cc
@@ -15,106 +15,135 @@ int min(int a,int b,int c)
     }
     return b;
 }
-
+            //  1234567
 // string stra("qwert12");
-// string strb("qwert");
+// string strb("1qwert");
 
 int comparestr1_ass(vector<vector<int>> &memo,string &stra,size_t starta,string &strb,size_t startb)
 {
+
     cout<<"compare: "<<starta<<" "<<startb<<endl;
     
     int a=INT32_MAX;
     int c=INT32_MAX;
     int b=INT32_MAX;
-    if(starta > stra.size())
+    
+ 
+
+    if(starta >= stra.size())
     {
-        if(startb > strb.size())
+        cout<<"___stra end"<<endl;
+        if(startb >= strb.size())
         {
-            memo[starta+1][startb+1]=0;
             return 0;
         } 
-        memo[starta][startb]=abs(static_cast<int>(stra.size()-strb.size()));
+        memo[starta][startb]=strb.size()-startb;
         return memo[starta][startb];
     }
-    if(startb > strb.size())
+    if(startb >= strb.size())
     {
-        if(starta > stra.size())
+        cout<<"___strb end"<<endl;
+        
+        if(starta >= stra.size())
         {
-            memo[starta+1][startb+1]=0;
             return 0;
         } 
-        memo[starta][startb]=abs(static_cast<int>(stra.size()-strb.size()));
+        memo[starta][startb]=stra.size()-starta;
         return memo[starta][startb];
     }
 
     //相等的情况
     if(stra[starta] == strb[startb])
     {
-        cout<<"equal: "<<starta<<" "<<startb<<endl;
+        // cout<<"equal: "<<starta<<" "<<startb<<endl;
         if(memo[starta+1][startb+1] != 0)
         {
+            memo[starta][startb]=memo[starta+1][startb+1];
             return memo[starta+1][startb+1];
         }else{
-            return comparestr1_ass(memo,stra,starta+1,strb,startb+1);
+            memo[starta][startb]=comparestr1_ass(memo,stra,starta+1,strb,startb+1);
+            return memo[starta+1][startb+1];
         }
     }
 
     //处理不相等的情况
-    cout<<"__unequal xiugai: "<<starta<<" "<<startb<<endl;
     
-    if(memo[starta+1][startb+1] != 0)
+    if(starta<stra.size() && startb<strb.size())
     {
-        a=memo[starta+1][startb+1];
-    }else{
-        a=comparestr1_ass(memo,stra,starta+1,strb,startb+1);
-    }
-    cout<<"__unequal zengjia: "<<starta<<" "<<startb<<endl;
-    
-    if(memo[starta+1][startb] != 0)
-    {
-        b=memo[starta+1][startb];
-    }else{
-        b=comparestr1_ass(memo,stra,starta+1,strb,startb);
-    }
-
-    cout<<"__unequal shanchu: "<<starta<<" "<<startb<<endl;
-    
-    if(memo[starta][startb+1] != 0)
-    {
-        c=memo[starta][startb+1];
-    }else{
-    
-        c=comparestr1_ass(memo,stra,starta,strb,startb+1);
-    }
-
-    int tmp;
-    tmp = min(a,b,c);
-
-    for(auto j:memo)
-    {
-        cout<<endl;
-        for(auto i:j)
+    // cout<<"__unequal xiugai: "<<starta<<" "<<startb<<endl;
+        if(memo[starta+1][startb+1] != 0)
         {
-            cout<<i<<"  ";
+            a=memo[starta+1][startb+1];
+        }else{
+            a=comparestr1_ass(memo,stra,starta+1,strb,startb+1);
         }
     }
-    cout<<endl;
-    cout<<endl;
+   
+    
+    if(starta<stra.size())
+    {
+    // cout<<"__unequal zengjia: "<<starta<<" "<<startb<<endl;
+    
+        if(memo[starta+1][startb] != 0)
+        {
+            b=memo[starta+1][startb];
+        }else{
+            b=comparestr1_ass(memo,stra,starta+1,strb,startb);
+        }
+    }
+ 
 
-    return tmp;
+    
+    if(startb<strb.size())
+    {
+
+        // cout<<"__unequal shanchu: "<<starta<<" "<<startb<<endl;
+    
+        if(memo[starta][startb+1] != 0)
+        {
+            c=memo[starta][startb+1];
+        }else{
+        
+            c=comparestr1_ass(memo,stra,starta,strb,startb+1);
+        }
+    }
+ 
+    
+    int tmp;
+    tmp = min(a,b,c);
+    memo[starta][startb]=tmp+1;
+    // cout<<"xiugai: "<<a<<" qita: "<<b<<" "<<c<<endl;
+    // cout<<endl<<"end of: "<<starta<<" "<<startb<<" value: "<<tmp+1<<endl;
+    
+
+    
+    // for(auto j:memo)
+    // {
+    //     cout<<endl;
+    //     for(auto i:j)
+    //     {
+    //         cout<<i<<"  ";
+    //     }
+    // }
+    // cout<<endl;
+    // cout<<endl;
+    // int tmp1;
+    // cin>>tmp1;
+
+    return tmp+1;
 }
 
 int comparestr1(string stra,string strb)
 {
-    vector<vector<int>> memo(stra.size()+1,vector<int>(strb.size()+1,0));
-    for(int i=0;i<=stra.size();i++)
-    {
-        memo[i][0]=i;
-    }
-    for(int i=0;i<=stra.size();i++)
-    {
-        memo[0][i]=i;
-    }
+    vector<vector<int>> memo(stra.size()+2,vector<int>(strb.size()+2,0));
+    // for(int i=0;i<=stra.size();i++)
+    // {
+    //     memo[i][0]=i;
+    // }
+    // for(int i=0;i<=strb.size();i++)
+    // {
+    //     memo[0][i]=i;
+    // }
 
     comparestr1_ass(memo,stra,0,strb,0);
     
@@ -131,9 +160,48 @@ int comparestr1(string stra,string strb)
     return 1;    
 }
 
-int main()
+
+int compareStr2(string stra,string strb)
 {
+    vector<vector<int> > memo(stra.size()+1,vector<int>(strb.size()+1,0));
+
+      for(int i=0;i<=stra.size();i++)
+    {
+        memo[i][0]=i;
+    }
+    for(int i=0;i<=strb.size();i++)
+    {
+        memo[0][i]=i;
+    }
+    for(int i=1;i<stra.size();i++)
+    {
+        for(int j=1;j<stra.size();j++)
+        {
+            if(stra[i-1]==strb[j-1])
+            {
+                memo[i][j]=memo[i-1][j-1];
+            }else{
+                memo[i][j]=min(memo[i-1][j],memo[i][j-1],memo[i-1][i-1])+1;
+            }
+        }
+    }
+
+        for(auto j:memo)
+    {
+        cout<<endl;
+        for(auto i:j)
+        {
+            cout<<i<<"  ";
+        }
+    }
+    cout<<endl;
+    cout<<endl;
+}
+
+int main()
+{           //   01234567
     string stra("qwert12");
-    string strb("qwert");
+    string strb("1qwert");
     comparestr1(stra,strb);
+    compareStr2(stra,strb);
 }
